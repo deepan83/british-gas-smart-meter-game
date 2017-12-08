a3508de139b4732c7aa6aee36d4d30f816cd3578
Richard Skene <richard@smackagency.com>
2017-12-08 10:14:15 +0000

    New character animations

--- src/components/views/Landing.vue
+++ src/components/views/Landing.vue
@@ -39,18 +39,22 @@ export default {
     }
   },
   mounted() {
-    setTimeout(() => {
-      this.interval = setInterval(() => {
-        this.typewritingTitle = this.title.slice(0, this.typewritingTitle.length + 1);
-        if (this.typewritingTitle.length == this.title.length) {
-          this.typewriting = false;
-          window.clearInterval(this.interval)
-          setTimeout(() => {
-            this.transition = true;
-          }, 2000);
-        }
-      }, 100);
-    }, 3000);
+    if (getQueryVariable('no_transition')) {
+      this.transition = true;
+    } else {
+      setTimeout(() => {
+        this.interval = setInterval(() => {
+          this.typewritingTitle = this.title.slice(0, this.typewritingTitle.length + 1);
+          if (this.typewritingTitle.length == this.title.length) {
+            window.clearInterval(this.interval)
+            setTimeout(() => {
+              this.typewriting = false;
+              this.transition = true;
+            }, 2000);
+          }
+        }, 100);
+      }, 3000);
+    }
   },
   computed: {
     ...mapGetters([
