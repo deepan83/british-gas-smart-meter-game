dfa6bd3e2c01eed2daa20a7cd56167de30ba707f
Richard Skene <richard@smackagency.com>
2018-01-02 12:46:44 +0000

    Obfuscated function

---
+++ Obfuscated.js
@@ -0,0 +1,19 @@
+// var w34545l12 = [
+//   'localhost:8080',
+//   'britishgas.co.uk',
+//   'bgsm.smack.build'
+// ];
+
+// function g2h112() {
+//   if (window.top == window.self) {
+//     return window.location.host;
+//   } else {
+//     return window.document.referrer.split('/')[2];
+//   }
+// }
+
+// window.h2c3 = function() {
+//     return w34545l12.some((element) => {
+//         return element.indexOf(g2h112()) !== -1;
+//     })
+// }
--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import Promise from 'es6-promise/auto';
 import store from './store'
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    template: '<App/>',
-    components: { App },
-    store
-})
+if (window.h2c3()) {
+  /* eslint-disable no-new */
+  new Vue({
+      el: '#app',
+      template: '<App/>',
+      components: { App },
+      store
+  })
+}
+
