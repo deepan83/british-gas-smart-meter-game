adeafffc94908929b6b374af1c40f911770142ec
Richard Skene <richard@smackagency.com>
2018-01-04 10:46:26 +0000

    Skip video font size increase

--- src/components/views/Video.vue
+++ src/components/views/Video.vue
@@ -92,7 +92,7 @@ export default {
   padding: 1vw 1vw 1vw 0;
   color: #007bc7;
   letter-spacing: 0.16vw;
-  font: 2vw/1 Gillsans;
+  font: 12px/1 Gillsans;
   border: 0;
   text-decoration: underline;
   background-color: transparent;
