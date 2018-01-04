8c8e9ee452fab94a0c877efc220e884847cb22b3
Richard Skene <richard@smackagency.com>
2018-01-04 11:31:26 +0000

    Animate score

--- src/components/ScoreHolder.vue
+++ src/components/ScoreHolder.vue
@@ -1,10 +1,39 @@
 <template>
-  <span class="score-holder">{{ score }}</span>
+  <span class="score-holder">{{ displayScore }}</span>
 </template>
 
 <script>
 export default {
-  props: ['score']
+  props: {score: {default: 0}},
+  data() {
+    return {
+      displayScore: 0,
+      interval: false
+    }
+  },
+  mounted() {
+    this.animateScore();
+  },
+  methods: {
+    animateScore() {
+      clearInterval(this.interval);
+      if(this.score == this.displayScore) {
+        return;
+      }
+      this.interval = setInterval(() => {
+        if (this.displayScore != this.score) {
+          var change = (this.score - this.displayScore) / 10;
+          change = change >= 0 ? Math.ceil(change) : Math.floor(change);
+          this.displayScore = this.displayScore + change;
+        }
+      }, 20);
+    }
+  },
+  watch: {
+    score() {
+      this.animateScore();
+    }
+  }
 }
 </script>
 
