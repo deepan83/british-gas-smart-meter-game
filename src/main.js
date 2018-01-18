a1a1767dd0ba781a48cc624b16dfa93e35548bbe
Richard Skene <richard@smackagency.com>
2018-01-18 13:06:28 +0000

    Browser sniffer

--- src/main.js
+++ src/main.js
@@ -4,13 +4,22 @@ import Promise from 'es6-promise/auto';
 import store from './store'
 import Vue2TouchEvents from 'vue2-touch-events'
 import VueYoutube from 'vue-youtube'
-
+import browserDetect from './util/browserDetect'
 Vue.config.productionTip = false
 
 Vue.use(VueYoutube)
 Vue.use(Vue2TouchEvents)
 
-if (window.h2c3()) {
+var browser = browserDetect();
+
+var nonsupportedBrowser = browser.name == 'Microsoft Internet Explorer' && browser.majorVersion <= 10;
+
+if (nonsupportedBrowser) {
+  var element = document.getElementById('nonsupported');
+  element.style.display = 'block';
+}
+
+if (window.h2c3() && !nonsupportedBrowser) {
   /* eslint-disable no-new */
   new Vue({
       el: '#app',
